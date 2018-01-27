8278d42c9735b38cf0efe985e857f18be2a7e224
Nur Muhammad <blog.nurmuhammad@gmail.com>
2018-01-27 13:00:04 +0700

    Adding news item layout

--- app/src/main/java/com/ngekoding/userinterface/ComponentActivity.java
+++ app/src/main/java/com/ngekoding/userinterface/ComponentActivity.java
@@ -1,6 +1,7 @@
 package com.ngekoding.userinterface;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 public class ComponentActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnToast, btnDialog, btnSnackbar;
+
     private Snackbar snackbar;
 
     @Override
@@ -20,45 +22,56 @@ public class ComponentActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_component);
 
+        // View binding
         btnToast = (Button) findViewById(R.id.btn_toast);
         btnSnackbar = (Button) findViewById(R.id.btn_snackbar);
         btnDialog = (Button) findViewById(R.id.btn_dialog);
 
         btnToast.setOnClickListener(this);
-        btnSnackbar.setOnClickListener(this);
         btnDialog.setOnClickListener(this);
+        btnSnackbar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.btn_toast:
+                // Tampilkan toast
+                Toast.makeText(this, "Hi, welcome friend...", Toast.LENGTH_LONG).show();
+                break;
             case R.id.btn_dialog:
-                showDialog();
+                // Tampilkan dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle("Delete Confirmation")
+                        .setMessage("Are you sure to delete this file?")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // Proses delete
+                                Toast.makeText(ComponentActivity.this, "File deleted!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
                 break;
             case R.id.btn_snackbar:
-                showSnackbar();
-                break;
-            case R.id.btn_toast:
-                showToast();
+                // Tampilkan snackbar
+                snackbar = Snackbar.make(btnSnackbar, "No internet connection", Snackbar.LENGTH_LONG);
+                snackbar.setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(ComponentActivity.this, "Reconnecting...", Toast.LENGTH_LONG).show();
+                    }
+                });
+                snackbar.show();
                 break;
         }
-    }
-
-    private void showDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("My Dialog")
-                .setMessage("Hello, welcome to my home");
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
-    private void showSnackbar() {
-        snackbar = Snackbar.make(btnSnackbar, "Hello", BaseTransientBottomBar.LENGTH_LONG);
-        snackbar.show();
-    }
-
-    private void showToast() {
-        Toast.makeText(this, "Hi, how are you?", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/ngekoding/userinterface/features/news/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.ngekoding.userinterface.features.news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ngekoding.userinterface.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
